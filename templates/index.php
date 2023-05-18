943635477b95b0ace9f696bd4901e0d1eab3e5e2
Andres Borda <ajborda4@misena.edu.co>
2023-05-18 07:27:20 -0500

    Set up de estable de vistas y assets

--- templates/index.php
+++ templates/index.php
@@ -1,20 +1,17 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Index</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
-  rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ"
+  <link
+  rel="stylesheet"
+  integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ"
+  href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
   crossorigin="anonymous">
+  <?= $this->loadAssets('index') ?>
 </head>
 <body>
-  <h1>Hola Mundo</h1>
-  <button
-  x-data="test"
-  @click="clicked"
-  class="btn btn-outline-primary">
-    Test Alpine
-  </button>
+  <?= $this->fetch("./partials/header.php") ?>
 </body>
 </html>
---
+++ templates/partials/header.php
@@ -0,0 +1,10 @@
+<header
+class="p-2 shadow bg-body">
+<div class="container d-flex border-bottom align-items-center mb-2">
+  <img
+  class="m-2" src="/img/logo_aso.png"
+  alt="logo-aso"
+  height="30">
+  <span class="m-0 fs-4">Cl&iacute;nica Asotrauma</span>
+</div>
+</header>
