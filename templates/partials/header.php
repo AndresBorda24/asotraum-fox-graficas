6a02290ad39ed2d2430359b4634eaf5371186daa
Andres Borda <ajborda4@misena.edu.co>
2023-06-02 10:21:41 -0500

    Ajustes rutas partials

--- templates/partials/header.php
+++ templates/partials/header.php
@@ -1,7 +1,7 @@
 <header class="p-2 mb-4 shadow bg-body">
   <div class="container d-flex border-bottom align-items-center mb-0">
     <img
-    class="m-2" src="/img/logo_aso.png"
+    class="m-2" src="<?= $this->asset("/img/logo_aso.png") ?>"
     alt="logo-aso"
     height="30">
     <span class="m-0 fs-4">Cl&iacute;nica Asotrauma</span>
--- templates/partials/loader.php
+++ templates/partials/loader.php
@@ -2,7 +2,7 @@
 class="vh-100 vw-100 d-none fixed-top bg-black bg-opacity-75 flex flex-column"
 style="z-index: 3000;">
     <div class="m-auto text-center">
-        <img src="/img/aso-loader.png" alt="loader" width="50">
+        <img src="<?= $this->asset("/img/aso-loader.png") ?>" alt="loader" width="50">
         <span class="text-light d-block">Cargando...</span>
     </div>
 </div>
