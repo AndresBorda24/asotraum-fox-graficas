e302eb0fe31bbaced4ace4b8a39c8abf38c1f914
AndresBorda24 <anjart24@gmail.com>
2023-08-10 08:42:11 -0500

    Auto select de las fechas al cargar

--- assets/partials/selectDates/select-by-month.js
+++ assets/partials/selectDates/select-by-month.js
@@ -16,6 +16,19 @@ export default () => ({
     ],
     currentYear: new Date().getFullYear(),
     /**
+     * Determina que fecha esta seccionada. Index 0 es el tipo y el index 1
+     * hace referencia al mes / semestre 7 trimestre
+    */
+    selected: [ "", 0 ],
+
+    init() {
+        const x = new Date;
+        x.setDate(0);
+
+        this.byMonth(x.getMonth());
+    },
+
+    /**
      * Despacha el evento de actualizacion de los selects de las fechas de
      * inicio y final
     */
@@ -29,6 +42,7 @@ export default () => ({
      * Despacha el evento basado en un mes
     */
     byMonth(month) {
+        this.selected = ["M", month];
         const start = new Date(this.currentYear, month, 1);
         const end   = new Date(this.currentYear, month + 1, 0);
 
@@ -38,6 +52,7 @@ export default () => ({
      * Despacha el evento basado en un trimestre
     */
     byTrimestre( t ) {
+        this.selected = ["T", t];
         const y = this.currentYear;
         const dates = [
             {start: y + '-01-01', end: y + '-03-31'},
@@ -55,6 +70,7 @@ export default () => ({
      * Despacha el evento basado en un semestre
     */
     bySemestre( s ) {
+        this.selected = ["S", s];
         const y = this.currentYear;
         const dates = [
             {start: y + '-01-01', end: y + '-06-30'},
@@ -77,5 +93,15 @@ export default () => ({
         const start = new Date(ctrl.setDate(1))
 
         return [start, end];
+    },
+
+    /**
+     * Determina si un boton ha sido seleccionado o no.
+    */
+    isSelected( tipo, n ) {
+        return (
+            this.selected[0] === tipo
+            & this.selected[1] === n - 1
+        );
     }
 });
