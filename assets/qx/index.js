504aea6a3523eaa4fb944231563612506b94d11b
AndresBorda24 <anjart24@gmail.com>
2024-04-25 08:17:13 -0500

    Gráfica de ocipacion

--- assets/qx/index.js
+++ assets/qx/index.js
@@ -3,6 +3,7 @@ import "../css/index.css";
 import Alpine from "alpinejs";
 import general from "./components/general";
 import medicos from "./components/medicos";
+import ocupacion from "./components/ocupacion";
 import datesHandler from "./components/dates-handler";
 import motivosCancelacion from "./components/motivos-cancelacion";
 
@@ -11,6 +12,7 @@ window.Alpine = import.meta.env.PROD ? undefined : Alpine;
 document.addEventListener("alpine:init", () => {
     Alpine.data("General", general);
     Alpine.data("Medicos", medicos);
+    Alpine.data("Ocupacion", ocupacion);
     Alpine.data("DatesHandler", datesHandler);
     Alpine.data("MotivosCancelacion", motivosCancelacion);
 });
--- templates/QX/index.php
+++ templates/QX/index.php
@@ -4,8 +4,9 @@
 ]) ?>
 
 <main class="py-4 mx-auto px-3 p-md-4 d-flex flex-column gap-4" style="max-width: 1200px;">
-  <div class="d-flex flex-column gap-2 d-md-grid" style="grid-template-columns: 1fr 1fr;">
+  <div class="d-flex flex-column gap-4 d-md-grid" style="grid-template-columns: 1fr 1fr;">
     <?= $this->fetch("./QX/partials/general.php") ?>
+    <?= $this->fetch("./QX/partials/ocupacion.php") ?>
   </div>
   <?= $this->fetch("./QX/partials/motivos-cancelacion.php") ?>
   <?= $this->fetch("./QX/partials/medicos.php") ?>
